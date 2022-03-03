74d39583c23f902424ab054b2d8737cbebbfd232
calebforks <99980111+calebforks@users.noreply.github.com>
2022-03-02 21:05:16 -0500

    Create unityUrls.js

---
+++ js/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getGameJsonUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/11/3e3a3e7f9b174542bcfb0fd7615b7725/WebGL.json";
+}
+
+function getGameLoaderUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/11/3e3a3e7f9b174542bcfb0fd7615b7725/UnityLoader.js";
+}
