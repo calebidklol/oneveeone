5c19dadd65ffb37faf7fdaea867f0a765785a88a
calebidklol <98755640+calebidklol@users.noreply.github.com>
2022-03-30 10:53:32 -0400

    Update unityUrls.js

--- js/unityUrls.js
+++ js/unityUrls.js
@@ -1,9 +1,9 @@
 window.loadedUrls = true;
 
 function getGameJsonUrl() {
-	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/11/3e3a3e7f9b174542bcfb0fd7615b7725/WebGL.json";
+	return 'https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/8/85164e58ab08443f8040e19ff4175bd8/WebGL.json';
 }
 
 function getGameLoaderUrl() {
-	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/11/3e3a3e7f9b174542bcfb0fd7615b7725/UnityLoader.js";
+	return 'https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/8/85164e58ab08443f8040e19ff4175bd8/UnityLoader.js';
 }
